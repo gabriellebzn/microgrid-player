1f57233ca423f53cedcb2b00a4d327d9bcd33314
gabriellebzn <gabrielle.bazin@eleves.enpc.fr>
2021-05-17 09:56:02 +0200

    Fonction prise de décision ferme solaire Bazin Sicard

--- player.py
+++ player.py
@@ -5,34 +5,31 @@ import pandas as pd
 
 
 class Player:
-    
-    
-
-
 
 	def __init__(self):
 		# some player might not have parameters
-		self.parameters = 0
+		self.parameters = 15
 		self.horizon = 48
-        self.surface = 1000
-        self.l_pv = np.zeros(48)
-        self.a = np.zeros(48)
-        self.C = 30000
-        self.rho_c = rho_d = 0.95
-        self.pmax = 10000
-        self.prix = np.random.rand(48)
-        self.T = 48
-        self.delta_t = 0.5
+		self.surface = 100
+		self.l_pv = np.zeros(48)
+		self.a = np.zeros(48)
+		self.C = 30000
+		self.rho_c = 0.95
+		self.rho_d = 0.95
+		self.pmax = 10000
+		self.prix = 100*np.random.rand(48)
+		self.T = 48
+		self.delta_t = 0.5
 
-    	df = pd.read_csv('pv_prod_scenarios.csv',sep=';')
-        df = df[(df['region']=='grand_nord') & (df['day']=='01/01/2014')]
-        prod = df[['pv_prod (W/m2)']]
-        l_pv = np.array(prod)
-        l_pv = (-1)*surface * l_pv
-        l_pv = np.repeat(l_pv,2)
+		df = pd.read_csv('pv_prod_scenarios.csv',sep=';')
+		df = df[(df['region']=='grand_nord') & (df['day']=='01/01/2014')]
+		self.prod = df[['pv_prod (W/m2)']]
+		self.l_pv = np.array(self.prod)
+		self.l_pv = (-1)*self.surface * self.l_pv /1000
+		self.l_pv = np.repeat(self.l_pv,2)
 
-        l_bat = np.zeros(48)
-        l_i = np.zeros(48)
+		self.l_bat = np.zeros(48)
+		self.l_i = np.zeros(48)
 
 	def set_scenario(self, scenario_data):
 		self.data = scenario_data
@@ -42,22 +39,27 @@ class Player:
 
 	def compute_all_load(self):
 		load = np.zeros(self.horizon)
-		# for time in range(self.horizon):
-		# 	load[time] = self.compute_load(time)
+		for time in range(self.horizon):
+			load[time] = self.compute_load(time)
 		return load
 
 	def take_decision(self, time):
-		lpv = l_pv[0:time]
-		a_prov = 0
-		min = 0
-		min_li = 0
-		for lbat in lpv:
-			a_prov = a[time - 1] + (rho_c*max(lbat,0) - (1/rho_d)*min(lbat,0))*delta_t
-			if (a_prov <= C) and (abs(lbat) <= pmax):
-				if (lbat - l_pv[time])*prix[time] < min :
-					min = (lbat - l_pv[time])*prix[time]
-					min_li = lbat - l_pv[time]
-		l_i[time] = min_li
+		lpv = self.l_pv
+		for t in range(time, 48):
+			a_prov = float(0)
+			mini = 0
+			min_li = 0
+			for i in range(len(lpv)):
+				if t >= 1:
+					a_prov = self.a[t - 1] + (self.rho_c*np.maximum(lpv[i],0) - (1/self.rho_d)*np.minimum(lpv[i],0))*self.delta_t
+				else :
+					a_prov = (self.rho_c * np.maximum(lpv[i], 0) - (1 / self.rho_d) * np.minimum(lpv[i], 0)) * self.delta_t
+				if (a_prov <= self.C) and (abs(lpv[i]) <= self.pmax):
+					if (lpv[i] - self.l_pv[t])*self.prix[t] < mini :
+						mini = (lpv[i] - self.l_pv[t])*self.prix[t]
+						min_li = lpv[i] - self.l_pv[t]
+			self.l_i[t] = min_li
+		#print(self.l_i)
 		return 0
 
 	def compute_load(self, time):
@@ -68,3 +70,7 @@ class Player:
 	def reset(self):
 		# reset all observed data
 		pass
+
+if __name__ == "__main__":
+	myplayer = Player()
+	myload = myplayer.compute_load(1)
