1a7b16755efe3736cd7ed0f1037cc75c9cb444fe
gabriellebzn <gabrielle.bazin@eleves.enpc.fr>
2021-05-27 21:28:34 +0200

    Fonction prise de décision ferme solaire améliorée Bazin Sicard

--- player.py
+++ player.py
@@ -3,6 +3,17 @@
 import numpy as np
 import pandas as pd
 
+# python 3
+# this class combines all basic features of a generic player
+
+import pulp
+import numpy as np
+import pandas as pd
+
+
+# import sys
+# import os
+# from linear_prog.pulp_utils import *
 
 class Player:
 
@@ -11,25 +22,34 @@ class Player:
 		self.parameters = 15
 		self.horizon = 48
 		self.surface = 100
-		self.l_pv = np.zeros(48)
-		self.a = np.zeros(48)
+		self.l_pv = {}
+		self.l_bat = {}
+		self.l_i = {}
+		self.a = {}
+
 		self.C = 30000
 		self.rho_c = 0.95
 		self.rho_d = 0.95
 		self.pmax = 10000
-		self.prix = 100*np.random.rand(48)
+		self.prix = 100 * np.random.rand(48)
 		self.T = 48
 		self.delta_t = 0.5
 
-		df = pd.read_csv('pv_prod_scenarios.csv',sep=';')
-		df = df[(df['region']=='grand_nord') & (df['day']=='01/01/2014')]
+		df = pd.read_csv('pv_prod_scenarios.csv', sep=';')
+		df = df[(df['region'] == 'grand_nord') & (df['day'] == '01/01/2014')]
 		self.prod = df[['pv_prod (W/m2)']]
 		self.l_pv = np.array(self.prod)
-		self.l_pv = (-1)*self.surface * self.l_pv /1000
-		self.l_pv = np.repeat(self.l_pv,2)
+		self.l_pv = (-1) * self.surface * self.l_pv / 1000
+		self.l_pv = np.repeat(self.l_pv, 2)
 
-		self.l_bat = np.zeros(48)
-		self.l_i = np.zeros(48)
+		for t in range(48):
+			self.l_bat[t] = pulp.LpVariable('lbat_' + str(t), -self.pmax, self.pmax)
+			# self.l_bat = pulp.LpVariable.dicts('l_bat', indexs=np.arange(48), lowBound=-1*self.pmax, upBound=self.pmax)
+			self.l_i[t] = pulp.LpVariable('li_' + str(t), None, None)
+			# self.l_i = pulp.LpVariable.dicts('l_i', indexs=np.arange(48))
+			self.a[t] = pulp.LpVariable('a_' + str(t), lowBound=0.0, upBound=self.C)
+
+	# self.a = pulp.LpVariable.dicts('a', indexs=np.arange(48), lowBound=0.0, upBound=self.C)
 
 	def set_scenario(self, scenario_data):
 		self.data = scenario_data
@@ -44,22 +64,33 @@ class Player:
 		return load
 
 	def take_decision(self, time):
+		lp = pulp.LpProblem("takedecision.lp", pulp.LpMinimize)
+		lp.setSolver()
 		lpv = self.l_pv
+
 		for t in range(time, 48):
-			a_prov = float(0)
-			mini = 0
-			min_li = 0
-			for i in range(len(lpv)):
-				if t >= 1:
-					a_prov = self.a[t - 1] + (self.rho_c*np.maximum(lpv[i],0) - (1/self.rho_d)*np.minimum(lpv[i],0))*self.delta_t
-				else :
-					a_prov = (self.rho_c * np.maximum(lpv[i], 0) - (1 / self.rho_d) * np.minimum(lpv[i], 0)) * self.delta_t
-				if (a_prov <= self.C) and (abs(lpv[i]) <= self.pmax):
-					if (lpv[i] - self.l_pv[t])*self.prix[t] < mini :
-						mini = (lpv[i] - self.l_pv[t])*self.prix[t]
-						min_li = lpv[i] - self.l_pv[t]
-			self.l_i[t] = min_li
-		#print(self.l_i)
+			if t == 0:
+				lp += self.a[t] == 0, "cnt_batt_" + str(t)
+			if t == 48:
+				lp += self.l_bat[t] == 48, "cnt_batt_" + str(t)
+			else:
+				# OB ajout d'un affichage -> bogue est ici... votre dictionnaire
+				# ne contient pas la clé 0 quand t= 1
+				# print("t=", t, "et dict. = ",  self.a)
+				lp += self.a[t] == self.a[t - 1] + (
+							self.rho_c * np.maximum(self.l_bat[t], 0) - (1 / self.rho_d) * np.minimum(self.l_bat[t],
+																									  0)) * self.delta_t, "cnt_batt_" + str(
+					t)
+			lp += self.l_i[t] == self.l_bat[t] - lpv[t], "cnt_li_" + str(t)
+
+		lp.setObjective(pulp.lpSum([self.l_i[t] * self.prix[t] for t in range(time, 48)]))
+
+		status = lp.solve()  # Solver
+		print(pulp.LpStatus[status])
+		print(self.l_i[t].varValue)
+		# model=Model(lp,self.l_i)
+
+		# return model
 		return 0
 
 	def compute_load(self, time):
@@ -71,6 +102,78 @@ class Player:
 		# reset all observed data
 		pass
 
+
 if __name__ == "__main__":
 	myplayer = Player()
-	myload = myplayer.compute_load(1)
+	myload = myplayer.compute_load(2)
+
+# class Player:
+#
+# 	def __init__(self):
+# 		# some player might not have parameters
+# 		self.parameters = 15
+# 		self.horizon = 48
+# 		self.surface = 100
+# 		self.l_pv = np.zeros(48)
+# 		self.a = np.zeros(48)
+# 		self.C = 30000
+# 		self.rho_c = 0.95
+# 		self.rho_d = 0.95
+# 		self.pmax = 10000
+# 		self.prix = 100*np.random.rand(48)
+# 		self.T = 48
+# 		self.delta_t = 0.5
+#
+# 		df = pd.read_csv('pv_prod_scenarios.csv',sep=';')
+# 		df = df[(df['region']=='grand_nord') & (df['day']=='01/01/2014')]
+# 		self.prod = df[['pv_prod (W/m2)']]
+# 		self.l_pv = np.array(self.prod)
+# 		self.l_pv = (-1)*self.surface * self.l_pv /1000
+# 		self.l_pv = np.repeat(self.l_pv,2)
+#
+# 		self.l_bat = np.zeros(48)
+# 		self.l_i = np.zeros(48)
+#
+# 	def set_scenario(self, scenario_data):
+# 		self.data = scenario_data
+#
+# 	def set_prices(self, prices):
+# 		self.prices = prices
+#
+# 	def compute_all_load(self):
+# 		load = np.zeros(self.horizon)
+# 		for time in range(self.horizon):
+# 			load[time] = self.compute_load(time)
+# 		return load
+#
+# 	def take_decision(self, time):
+# 		lpv = self.l_pv
+# 		for t in range(time, 48):
+# 			a_prov = float(0)
+# 			mini = 0
+# 			min_li = 0
+# 			for i in range(len(lpv)):
+# 				if t >= 1 and t<len(lpv)-1:
+# 					a_prov = self.a[t - 1] + (self.rho_c*np.maximum(lpv[i],0) - (1/self.rho_d)*np.minimum(lpv[i],0))*self.delta_t
+# 				else :
+# 					a_prov = 0
+# 				if (a_prov <= self.C) and (abs(lpv[i]) <= self.pmax):
+# 					if (lpv[i] - self.l_pv[t])*self.prix[t] < mini :
+# 						mini = (lpv[i] - self.l_pv[t])*self.prix[t]
+# 						min_li = lpv[i] - self.l_pv[t]
+# 			self.l_i[t] = min_li
+# 		#print(self.l_i)
+# 		return 0
+#
+# 	def compute_load(self, time):
+# 		load = self.take_decision(time)
+# 		# do stuff ?
+# 		return load
+#
+# 	def reset(self):
+# 		# reset all observed data
+# 		pass
+#
+# if __name__ == "__main__":
+# 	myplayer = Player()
+# 	myload = myplayer.compute_load(1)
